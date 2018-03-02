fd18b9ba9bc870408f278d41b3b24cf2c064b418
derrick <derrick.2a4@gmail.com>
2018-03-02 20:43:55 +0800

    Implement logging in

--- web/login.php
+++ web/login.php
@@ -102,7 +102,8 @@ span.psw {
 
         if ($valid > 0) {
             $data = pg_fetch_row($check);
-        echo "<script type='text/javascript'>alert('$_POST[psw]');</script>";
+        echo "<script type='text/javascript'>alert('$data[0]');</script>";
+        echo "<script type='text/javascript'>alert('password_verify($_POST[psw],$data[0])');</script>";
 
             if (password_verify($_POST[psw], $data[0])) {
                 if ($data[1] == True) {
