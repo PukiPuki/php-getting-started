cdd4a2ba7aad13437501e0846d508f00f5c5d84c
derrick <derrick.2a4@gmail.com>
2018-03-02 19:13:05 +0800

    Try stored procedure

--- web/index.php
+++ web/index.php
@@ -33,10 +33,10 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
     $pg_conn = pg_connect(pg_connection_string_from_database_url())
         or die('Could not connect:' . pg_last_error());
     # $query = 'SELECT * FROM auth_user';
-    $query = 'SELECT admin_select_user()';
+    $query = 'SELECT admin_select_items()';
     $result = pg_query($pg_conn, $query) or die('Query failed: '. pg_last_error());
     $data = pg_fetch_assoc($result);
-    echo $data[username];
+    echo $data["owner"];
 ?>
 </div>
 
