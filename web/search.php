e9762f2a997bbd80fdecf1bbaf2289c82f695b10
DF <dfwong5566@gmail.com>
2018-03-02 23:26:12 +0800

    search_4

--- web/search.php
+++ web/search.php
@@ -42,13 +42,15 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
       pg_send_query($pg_conn, $query) or die('Query failed: '. pg_last_error());
       $result = pg_get_result($pg_conn);
       
+      echo $result;
+
       if ($result) {
           $state = pg_result_error_field($result, PGSQL_DIAG_SQLSTATE);
       }
     }
 ?>
 <h1 class="w3-text-teal">Search</h1>
-      
+
  <form action="search.php" method="GET">
     <button type="submit" name="get_all_button" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
       Get All</button>
