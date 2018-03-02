460b27b98bafa5175ffa7b51df6708e15e07924e
derrick <derrick.2a4@gmail.com>
2018-03-03 01:02:57 +0800

    Fix admin

--- web/admin.php
+++ web/admin.php
@@ -100,7 +100,9 @@ span.psw {
 
 
     if (isset($_POST['edit_user'])) {
-        echo "<script type='text/javascript'>alert('{$POST[isAdmin]}');</script>";
+        echo "<script type='text/javascript'>alert('{$_POST[username]}');</script>";
+        echo "<script type='text/javascript'>alert('{$_POST[newphone]}');</script>";
+        echo "<script type='text/javascript'>alert('{$_POST[isAdmin]}');</script>";
         $pg_conn = pg_connect(pg_connection_string_from_database_url());
         $query = "SELECT admin_edit_user('$_POST[username]', '$_POST[newphone]', 'True'";
         $result = pg_query($pg_conn, $query) or die('Query Failed' . pg_last_error());
