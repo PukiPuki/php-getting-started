48815409451c54fc9e1af1f1e24481c3f1202477
DF <dfwong5566@gmail.com>
2018-03-03 00:10:58 +0800

    Search_9

--- push.sh
+++ push.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+search_count=`cat search_count.txt`
+inc="$((search_count+1))"
+echo "Commiting to Search_$inc"
 git add -A
-git commit -m $1
+git commit -m "Search_$inc"
 git push heroku master
