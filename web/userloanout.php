60f42fda777a5648dd5cf4e0a7b091f47be31aa2
LimYangSheng <limyangsheng1995@gmail.com>
2018-03-03 01:07:42 +0800

    debug

--- web/userloanout.php
+++ web/userloanout.php
@@ -34,7 +34,7 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
     $pg_conn = pg_connect(pg_connection_string_from_database_url())
         or die('Could not connect:' . pg_last_error());
     //$query = "SELECT * FROM all_current_loans_accepted('$username')";
-    $query = "SELECT * FROM users";
+    $query = "SELECT * FROM bids";
     $result = pg_query($pg_conn, $query) or die('Query failed: '. pg_last_error());
 
     if(!$result) {
