4b470fc868e1196b31d70950c8ad25dfb1cd3827
derrick <derrick.2a4@gmail.com>
2018-03-02 22:11:35 +0800

    Update transaction viewing

--- web/navbar.php
+++ web/navbar.php
@@ -4,15 +4,15 @@
     <a href="index.php" class="w3-bar-item w3-button w3-theme-l1">Stuffsharing</a>
     <!-- Right-sided navbar links -->
     <div class="w3-right w3-hide-small">
+    <a href="search.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Search</a>
     <?php
         if (!isset($_SESSION['user'])) {
             echo '<a href="login.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Login</a>
     <a href="signup.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Sign Up</a>';
     } else {
-
+        echo '<a href="logout.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Logout</a>';
     }
     ?>
-    <a href="search.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Search</a>
     </div>
   </div>
 </div>
