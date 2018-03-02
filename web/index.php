3bd6ed4f34cb849c26af016f21862f8222b75f3d
derrick <derrick.2a4@gmail.com>
2018-03-02 16:55:20 +0800

    test

--- web/index.php
+++ web/index.php
@@ -17,6 +17,15 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
 <?php
     include 'navbar.php';
 ?>
+<!-- Header with full-height image -->
+<header class="bgimg-1 w3-display-container w3-grayscale-min" id="home">
+  <div class="w3-display-left w3-text-black" style="padding:48px">
+    <span class="w3-jumbo w3-hide-small">Start something that matters!</span><br>
+    <span class="w3-xxlarge w3-hide-large w3-hide-medium">Start something that matters</span><br>
+    <span class="w3-large">Stop doing everything yourself and get help from others.</span>
+    <p><a href="Registration.php" class="w3-button w3-black w3-padding-large w3-large w3-margin-top w3-opacity w3-hover-opacity-off">Register now and start today</a></p>
+  </div>
+</header>
 
 <div class="container">
 
