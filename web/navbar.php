f9c83344a5828e1a0df7d0d81761e4ef332b826b
derrick <derrick.2a4@gmail.com>
2018-03-02 22:05:36 +0800

    Update navbar to hide upon login

--- web/navbar.php
+++ web/navbar.php
@@ -4,8 +4,14 @@
     <a href="index.php" class="w3-bar-item w3-button w3-theme-l1">Stuffsharing</a>
     <!-- Right-sided navbar links -->
     <div class="w3-right w3-hide-small">
-    <a href="login.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Login</a>
-    <a href="signup.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Sign Up</a>
+    <?php
+        if (!isset($_SESSION['user'])) {
+            echo '<a href="login.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Login</a>
+    <a href="signup.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Sign Up</a>';
+    } else {
+
+    }
+    ?>
     <a href="search.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Search</a>
     </div>
   </div>
