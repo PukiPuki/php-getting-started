cc0f1b8b15bd800531993b24ff231e4def8f0c07
derrick <derrick.2a4@gmail.com>
2018-03-02 21:41:27 +0800

    Update index php

--- web/base.php
+++ web/base.php
@@ -22,10 +22,8 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
 
 <div style=margin-top:43px>
     <div class="w3-container">
-        <h1 class="w3-text-teal">Welcome</h1>
+        <h1 class="w3-text-teal">Heading</h1>
     </div>
-
-<div>
 
 <?php
     function pg_connection_string_from_database_url() {
@@ -34,9 +32,11 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
     }
     $pg_conn = pg_connect(pg_connection_string_from_database_url())
         or die('Could not connect:' . pg_last_error());
-    echo $_SESSION[username];
+    $query = 'SELECT * FROM auth_user';
+    $result = pg_query($pg_conn, $query) or die('Query failed: '. pg_last_error());
+    $data = pg_fetch_assoc($result);
+    echo $data[username];
 ?>
-</div>
 </div>
 
 
