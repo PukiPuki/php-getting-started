f135bd7dc159b7b536f68d8fa9f145712180538f
derrick <derrick.2a4@gmail.com>
2018-03-02 22:47:31 +0800

    Restrict admin panel

--- web/admin.php
+++ web/admin.php
@@ -87,10 +87,17 @@ span.psw {
 <body>
 <?php
     include 'navbar.php';
+
     function pg_connection_string_from_database_url() {
         extract(parse_url($_ENV["DATABASE_URL"]));
         return "user=$user password=$pass host=$host dbname=" . substr($path, 1) . " sslmode=require"; # <- you may want to add sslmode=require there too
     }
+    if (!$_SESSION[isAdmin]){
+        header('Location:index.php');
+    } else {
+
+    }
+
 
     if (isset($_POST['edit_user'])) {
         $pg_conn = pg_connect(pg_connection_string_from_database_url());
