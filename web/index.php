f0d4319c0504c395142e831ca9633d0e35fb1fcf
derrick <derrick.2a4@gmail.com>
2018-03-03 13:38:58 +0800

    Add MUI CSS

--- web/index.php
+++ web/index.php
@@ -6,12 +6,12 @@
 <title>Stuff Sharing (CS2102 Project)</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css"
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<!-- Compiled and minified CSS -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css"
 <style>
 html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
 </style>
@@ -37,7 +37,7 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
     
     if (isset($_SESSION[user])) {
     echo '<div>
-    <div class="w3-container">
+    <div>
         <h2 class="w3-text-teal">Active transactions</h2>
     </div>';
         $query = "SELECT * FROM select_active_transactions()";
