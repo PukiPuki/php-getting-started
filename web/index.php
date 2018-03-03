05ed8cb780d52c4faca0d8ad438d0ef3a8c9b276
derrick <derrick.2a4@gmail.com>
2018-03-03 13:48:19 +0800

    Add MUI CSS

--- web/index.php
+++ web/index.php
@@ -31,7 +31,7 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
     $pg_conn = pg_connect(pg_connection_string_from_database_url())
         or die('Could not connect:' . pg_last_error());
     echo " <div style=margin-top:43px>
-    <div>
+    <div class=\"w3-container\">
         <h1 class=\"w3-text-teal\">Welcome {$_SESSION[user]}</h1>
     </div>";
     
