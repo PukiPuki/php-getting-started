c25baf3e5acc0c29b9f3a610cb0fc9fe95a6811d
derrick <derrick.2a4@gmail.com>
2018-03-03 12:19:26 +0800

    Display transactions upon login

--- web/index.php
+++ web/index.php
@@ -20,7 +20,7 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
 ?>
 </div>
 
-
+<div class="container">
 <?php
     function pg_connection_string_from_database_url() {
         extract(parse_url($_ENV["DATABASE_URL"]));
@@ -32,8 +32,50 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
     <div class=\"w3-container\">
         <h1 class=\"w3-text-teal\">Welcome {$_SESSION[user]}</h1>
     </div>";
+    
+    if (isset($_SESSION[user])) {
+        $query = "SELECT * FROM select_active_transactions()";
+        $result = pg_query($pg_conn, $query) or die('Query failed: '. pg_last_error());
+        if (!$result) {
+            $message = ' <p>There are no transactions in the database!</p> </div> </div> </div>';
+            echo "<script type='text/javascript'>alert('$message');</script>";
+        } else {
+             $index = 1;
+                  echo '
+                <table style = "width:100%", align="center">
+                        <tr>
+                        <th>S/N</th>
+                        <th>tID</th>
+                        <th>Item</th>
+                        <th>Location</th>
+                        <th>Loan Date</th>
+                        <th>Return Date</th>
+                        <th>Owner</th>
+                        <th>Category</th>
+                        <th>Minimum Bid</th>
+                        <th>Automatic Bid</th>
+                        <th>Current Bid</th>
+                        </tr>';
+                while($row = pg_fetch_assoc($result)) {   //Creates a loop to loop through results
+                    echo '<tr align = "center">
+                    <td>'.$index.'</td>
+                    <td>'.$row["tid"].'</td>
+                    <td>'.$row["itemname"].'</td>
+                    <td>'.$row["location"].'</td>
+                    <td>'.$row["pickupdate"].'</td>
+                    <td>'.$row["returndate"].'</td>
+                    <td>'.$row["owner"].'</td>
+                    <td>'.$row["category"].'</td>
+                    <td>'.$row["minbid"].'</td>
+                    <td>'.$row["autobuy"].'</td>
+                    <td>'.$row["highbid"].'</td>
+                    </tr>';
+                    $index++;
+              }
+              echo '</table>';
+        }
+    }
 ?>
-</div>
 </div>
 
 
