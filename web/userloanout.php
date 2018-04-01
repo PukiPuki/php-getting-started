95d9b8af9db805f2c1a868f1bb5bf92e9c4fcd60
LimYangSheng <limyangsheng1995@gmail.com>
2018-04-01 14:40:29 +0800

    Search_27

--- web/userloanout.php
+++ web/userloanout.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <?php
-    session_start();
-    $username = $_SESSION[user];
+session_start();
+$username = $_SESSION[user];
 ?>
 <html>
 <title>Stuff Sharing (CS2102 Project)</title>
@@ -12,13 +12,15 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
-html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
+    html, body, h1, h2, h3, h4, h5, h6 {
+        font-family: "Roboto", sans-serif;
+    }
 </style>
 <body>
 <div>
-<?php
+    <?php
     include 'navbar.php';
-?>
+    ?>
 </div>
 
 <div style=margin-top:43px>
@@ -26,26 +28,27 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
         <h1 class="w3-text-teal">Your Loan</h1>
     </div>
 
-<?php
-    function pg_connection_string_from_database_url() {
+    <?php
+    function pg_connection_string_from_database_url()
+    {
         extract(parse_url($_ENV["DATABASE_URL"]));
         return "user=$user password=$pass host=$host dbname=" . substr($path, 1) . " sslmode=require"; # <- you may want to add sslmode=require there too
     }
+
     $pg_conn = pg_connect(pg_connection_string_from_database_url())
-        or die('Could not connect:' . pg_last_error());
+    or die('Could not connect:' . pg_last_error());
     $query = "SELECT * FROM all_current_loans_accepted('$username')";
-    $result = pg_query($pg_conn, $query) or die('Query failed: '. pg_last_error());
+    $result = pg_query($pg_conn, $query) or die('Query failed: ' . pg_last_error());
 
     echo '<div>
     <div class="w3-container">
         <h2 class="w3-text-teal">Successful Loans</h2>
     </div>';
 
-    if(!$result) {
+    if (!$result) {
         $message = '<p>You have nothing loaned out!</p>';
         echo "<script type='text/javascript'>alert('$message');</script>";
-    }
-    else {
+    } else {
         $index = 1;
         echo '
             <table class="striped responsive-table centered highlight", style="width:100%">
@@ -55,13 +58,13 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
             <th>Bidder</th>
             <th>Return Date</th>
             </tr>';
-        while($row = pg_fetch_assoc($result)) {
-    	   echo '<tr align = "center">
-                <td>'.$index.'</td>
-    			<td>'.$row["itemname"].'</td>
-                <td>'.$row["biddername"].'</td>
-                <td>'.$row["returndate"].'</td>
-    		';
+        while ($row = pg_fetch_assoc($result)) {
+            echo '<tr align = "center">
+                <td>' . $index . '</td>
+    			<td>' . $row["itemname"] . '</td>
+                <td>' . $row["biddername"] . '</td>
+                <td>' . $row["returndate"] . '</td>
+    		    </tr>';
             $index++;
         }
         echo '</table>';
@@ -73,13 +76,12 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
     </div>';
 
     $query = "SELECT * FROM all_current_loans_pending('$username')";
-    $result = pg_query($pg_conn, $query) or die('Query failed: '. pg_last_error());
+    $result = pg_query($pg_conn, $query) or die('Query failed: ' . pg_last_error());
 
-    if(!$result) {
+    if (!$result) {
         $message = '<p>You have no pending loans!</p>';
         echo "<script type='text/javascript'>alert('$message');</script>";
-    }
-    else {
+    } else {
         $index = 1;
         echo '
             <table class="striped responsive-table centered highlight", style="width:100%">
@@ -96,37 +98,37 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
             <th>Bidder Name</th>
             <th>Current Bid</th>
             </tr>';
-        while($row = pg_fetch_assoc($result)) {
-           echo '<tr align = "center">
-                <td>'.$index.'</td>
-                <td>'.$row["itemid"].'</td>
-                <td>'.$row["itemname"].'</td>
-                <td>'.$row["category"].'</td>
-                <td>'.$row["minbid"].'</td>
-                <td>'.$row["autobuy"].'</td>
-                <td>'.$row["location"].'</td>
-                <td>'.$row["pickupdate"].'</td>
-                <td>'.$row["returndate"]'</td>
-                <td>'.$row["biddername"].'</td>
-                <td>'.$row["maxbid"].'</td>
-            ';
+        while ($row = pg_fetch_assoc($result)) {
+            echo '<tr align = "center">
+                <td>' . $index . '</td>
+                <td>' . $row["itemid"] . '</td>
+                <td>' . $row["itemname"] . '</td>
+                <td>' . $row["category"] . '</td>
+                <td>' . $row["minbid"] . '</td>
+                <td>' . $row["autobuy"] . '</td>
+                <td>' . $row["location"] . '</td>
+                <td>' . $row["pickupdate"] . '</td>
+                <td>' . $row["returndate"]'</td>
+                <td>' . $row["biddername"] . '</td>
+                <td>' . $row["maxbid"] . '</td>
+                </tr>';
             $index++;
         }
         echo '</table>';
     }
-?>
+    ?>
 
-<script type="text/javascript">
-      var query = "<?php echo $query ?>"
-</script>
+    <script type="text/javascript">
+        var query = "<?php echo $query ?>"
+    </script>
 
 </div>
 
 
 <script>
 
-// Get the DIV with overlay effect
-var overlayBg = document.getElementById("myOverlay");
+    // Get the DIV with overlay effect
+    var overlayBg = document.getElementById("myOverlay");
 
 </script>
 </body>
