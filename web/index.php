961f8dfd03db9e67ef124905d6772003df8b5840
pukipuki <dfwong5566@gmail.com>
2018-04-01 16:23:17 +0800

    Search_14

--- web/index.php
+++ web/index.php
@@ -156,7 +156,7 @@ if (isset($_POST['bid'])) {
     echo "<script type='text/javascript'>alert('$_POST[new_bid]');</script>";
     echo "<script type='text/javascript'>alert('$_SESSION[user]');</script>";
 
-    $query = "SELECT * FROM make_bid('$_POST[new_bid]', $_POST[tid], '$_SESSION.[user]')";
+    $query = "SELECT * FROM make_bid('$_POST[new_bid]', '$_POST[tid]', '$_SESSION[user]')";
     $result = pg_query($pg_conn, $query) or die('Query failed: ' . pg_last_error());
     if (!$result) {
         $message = ' <p>There are no transactions of that category</p> </div> </div> </div>';
