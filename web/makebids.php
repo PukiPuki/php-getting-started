bad858b27331745c78661b4d552157a74db86529
pukipuki <dfwong5566@gmail.com>
2018-04-01 16:39:39 +0800

    Search_16

--- web/makebids.php
+++ web/makebids.php
@@ -4,10 +4,10 @@ function makeBidInput($string)
     return
         '
             <form action="index.php" method="POST">
-                     <input type="text" placeholder="' . $string . '" name="bid" required>
-                     <button type="submit" name="bid" value="' . $string . '">Bid</button>
-                </form>
-            ';
+                 <input type="text" placeholder="' . $string . '" name="new_bid" required>
+                 <button type="submit" name="tid" value="' . $string . '">Bid</button>
+            </form>
+        ';
 }
 
 ?>
