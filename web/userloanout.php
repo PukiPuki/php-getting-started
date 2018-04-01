216f8c81a9f1115fa6ecfd9d20f26b8b92e9b6fc
derrick <derrick.2a4@gmail.com>
2018-04-02 00:33:01 +0800

    Debug

--- web/userloanout.php
+++ web/userloanout.php
@@ -130,17 +130,13 @@ echo '<div>
     }
 
     if (isset($_POST['biddername'])) {
-        $query = 'SELECT * FROM accept_loan('.$_POST[newtransactionid].','.$_POST[biddername].')';
-        $query2 = "SELECT * FROM accept_loan('$_POST[newtransactionid]','$_POST[biddername]')";
+        $query = 'SELECT * FROM accept_loan('.$_POST[newtransactionid].',\''.$_POST[biddername].'\')';
         $result = pg_send_query($pg_conn, $query) or die('Query failed: ' . pg_last_error());
         if (!$result) {
             echo "<script type='text/javascript'>
                 alert('Error in accepting loan!');
             </script>";
         } else {
-        echo "<script type='text/javascript'>
-                alert('Hi?');
-            </script>";
         echo "<script type='text/javascript'>
                 alert('$query');
             </script>";
