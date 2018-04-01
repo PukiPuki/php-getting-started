c4165a342908f48166c16f7732494b028195d458
pukipuki <dfwong5566@gmail.com>
2018-04-01 16:11:15 +0800

    Search_11

--- web/index.php
+++ web/index.php
@@ -155,14 +155,14 @@ if (isset($_POST['bid'])) {
     echo "<script type='text/javascript'>alert('$_POST[bid]');</script>";
     echo "<script type='text/javascript'>alert('$_POST[new_bid]');</script>";
     echo "<script type='text/javascript'>alert('$_SESSION.[user]');</script>";
-    $query = "SELECT * FROM make_bid('$_POST[bid]', '$_SESSION.[user]')";
 
-    $result = pg_query($pg_conn, $query) or die('Query failed: ' . pg_last_error());
-    if (!$result) {
-        $message = ' <p>There are no transactions of that category</p> </div> </div> </div>';
-        echo "<script type='text/javascript'>alert('$_POST[bid]');</script>";
-        echo "<script type='text/javascript'>alert('$_POST[bid]');</script>";
-    }
+    #$query = "SELECT * FROM make_bid('$_POST[bid]', '$_SESSION.[user]')";
+    #$result = pg_query($pg_conn, $query) or die('Query failed: ' . pg_last_error());
+    #if (!$result) {
+    #    $message = ' <p>There are no transactions of that category</p> </div> </div> </div>';
+    #    echo "<script type='text/javascript'>alert('$_POST[bid]');</script>";
+    #    echo "<script type='text/javascript'>alert('$_POST[bid]');</script>";
+    #}
 }
 
 function makeBidInput($string) {
