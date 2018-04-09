7a24a28b45dad93e27605fe961d438bad53c3e7e
derrick <derrick.2a4@gmail.com>
2018-04-09 15:32:29 +0800

    Update login

--- web/login.php
+++ web/login.php
@@ -6,14 +6,11 @@
 
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <!-- Compiled and minified CSS -->
-<link rel="stylesheet" href="./style/css/materialize.css"
-<link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet" type="text/css">
+<link type="text/css" rel="stylesheet" href="./style/css/materialize.css"media="screen,projection" />
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 <body>
@@ -81,5 +78,7 @@
 <?php
     echo $message;
 ?>
+ <!--JavaScript at end of body for optimized loading-->
+      <script type="text/javascript" src="./style/js/materialize.min.js"></script>
 </body>
 </html>
