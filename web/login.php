db8b89cae48a637560d57c2924dc5fe7c8b7bf19
derrick <derrick.2a4@gmail.com>
2018-04-09 15:23:43 +0800

    Update login

--- web/login.php
+++ web/login.php
@@ -15,80 +15,6 @@
 <link rel="stylesheet" href="./style/css/materialize.css"
 <link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet" type="text/css">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-/* <style> */
-/* form { */
-/*     border: 3px solid #f1f1f1; */
-/* } */
-
-/* /1* Full-width inputs *1/ */
-/* input[type=text], input[type=password] { */
-/*     width: 100%; */
-/*     padding: 12px 20px; */
-/*     margin: 8px 0; */
-/*     display: inline-block; */
-/*     border: 1px solid #ccc; */
-/*     box-sizing: border-box; */
-/* } */
-
-/* /1* Set a style for all buttons *1/ */
-/* button { */
-/*     background-color: #4CAF50; */
-/*     color: white; */
-/*     padding: 14px 20px; */
-/*     margin: 8px 0; */
-/*     border: none; */
-/*     cursor: pointer; */
-/*     width: 100%; */
-/* } */
-
-/* /1* Add a hover effect for buttons *1/ */
-/* button:hover { */
-/*     opacity: 0.8; */
-/* } */
-
-/* /1* Extra style for the cancel button (red) *1/ */
-/* .cancelbtn { */
-/*     width: auto; */
-/*     padding: 10px 18px; */
-/*     background-color: #f44336; */
-/* } */
-
-/* /1* Center the avatar image inside this container *1/ */
-/* .imgcontainer { */
-/*     text-align: center; */
-/*     margin: 24px 0 12px 0; */
-/* } */
-
-/* /1* Avatar image *1/ */
-/* img.avatar { */
-/*     width: 40%; */
-/*     border-radius: 50%; */
-/* } */
-
-/* /1* Add padding to containers *1/ */
-/* .container { */
-/*     padding: 16px; */
-/* } */
-
-/* /1* The "Forgot password" text *1/ */
-/* span.psw { */
-/*     float: right; */
-/*     padding-top: 16px; */
-/* } */
-
-/* /1* Change styles for span and cancel button on extra small screens *1/ */
-/* @media screen and (max-width: 300px) { */
-/*     span.psw { */
-/*         display: block; */
-/*         float: none; */
-/*     } */
-/*     .cancelbtn { */
-/*         width: 100%; */
-/*     } */
-/* } */
-
-/* </style> */
 </head>
 <body>
 <?php
@@ -135,10 +61,12 @@
     <input type="password" placeholder="Enter Password" name="psw" class="validate">
     </div>
     <div class="row">
-        <span class="psw">Forgot <a href="#">password?</a></span>
+        <p>
         <label>
-            <input type="checkbox" name="remember"> Remember me
+            <input type="checkbox" name="remember" /> 
+            <span> Remember me  </span>
         </label>
+        </p>
     </div>
 
     <div class="row">
