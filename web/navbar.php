453bf0728843de36820923b08dfb051f5b77834e
derrick <derrick.2a4@gmail.com>
2018-04-09 14:55:41 +0800

    Add navbar

--- web/navbar.php
+++ web/navbar.php
@@ -1,3 +1,13 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!-- Compiled and minified CSS -->
+<link rel="stylesheet" href="./style/css/materialize.css"
+<link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+
 <!-- Navbar -->
 <nav class="light-blue lighten-1" role="navigation">
     <div class="nav-wrapper"><a id="logo-container" href="#" class="brand-logo left">Stuffsharing</a>
