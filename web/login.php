5d56a34c9dea2207caf7cbfe59fbe38c0981c48a
derrick <derrick.2a4@gmail.com>
2018-04-09 15:15:19 +0800

    Change login

--- web/login.php
+++ web/login.php
@@ -7,10 +7,7 @@
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-<meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -19,79 +16,79 @@
 <link href="https://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet" type="text/css">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-<style>
-form {
-    border: 3px solid #f1f1f1;
-}
+/* <style> */
+/* form { */
+/*     border: 3px solid #f1f1f1; */
+/* } */
 
-/* Full-width inputs */
-input[type=text], input[type=password] {
-    width: 100%;
-    padding: 12px 20px;
-    margin: 8px 0;
-    display: inline-block;
-    border: 1px solid #ccc;
-    box-sizing: border-box;
-}
+/* /1* Full-width inputs *1/ */
+/* input[type=text], input[type=password] { */
+/*     width: 100%; */
+/*     padding: 12px 20px; */
+/*     margin: 8px 0; */
+/*     display: inline-block; */
+/*     border: 1px solid #ccc; */
+/*     box-sizing: border-box; */
+/* } */
 
-/* Set a style for all buttons */
-button {
-    background-color: #4CAF50;
-    color: white;
-    padding: 14px 20px;
-    margin: 8px 0;
-    border: none;
-    cursor: pointer;
-    width: 100%;
-}
+/* /1* Set a style for all buttons *1/ */
+/* button { */
+/*     background-color: #4CAF50; */
+/*     color: white; */
+/*     padding: 14px 20px; */
+/*     margin: 8px 0; */
+/*     border: none; */
+/*     cursor: pointer; */
+/*     width: 100%; */
+/* } */
 
-/* Add a hover effect for buttons */
-button:hover {
-    opacity: 0.8;
-}
+/* /1* Add a hover effect for buttons *1/ */
+/* button:hover { */
+/*     opacity: 0.8; */
+/* } */
 
-/* Extra style for the cancel button (red) */
-.cancelbtn {
-    width: auto;
-    padding: 10px 18px;
-    background-color: #f44336;
-}
+/* /1* Extra style for the cancel button (red) *1/ */
+/* .cancelbtn { */
+/*     width: auto; */
+/*     padding: 10px 18px; */
+/*     background-color: #f44336; */
+/* } */
 
-/* Center the avatar image inside this container */
-.imgcontainer {
-    text-align: center;
-    margin: 24px 0 12px 0;
-}
+/* /1* Center the avatar image inside this container *1/ */
+/* .imgcontainer { */
+/*     text-align: center; */
+/*     margin: 24px 0 12px 0; */
+/* } */
 
-/* Avatar image */
-img.avatar {
-    width: 40%;
-    border-radius: 50%;
-}
+/* /1* Avatar image *1/ */
+/* img.avatar { */
+/*     width: 40%; */
+/*     border-radius: 50%; */
+/* } */
 
-/* Add padding to containers */
-.container {
-    padding: 16px;
-}
+/* /1* Add padding to containers *1/ */
+/* .container { */
+/*     padding: 16px; */
+/* } */
 
-/* The "Forgot password" text */
-span.psw {
-    float: right;
-    padding-top: 16px;
-}
+/* /1* The "Forgot password" text *1/ */
+/* span.psw { */
+/*     float: right; */
+/*     padding-top: 16px; */
+/* } */
 
-/* Change styles for span and cancel button on extra small screens */
-@media screen and (max-width: 300px) {
-    span.psw {
-        display: block;
-        float: none;
-    }
-    .cancelbtn {
-        width: 100%;
-    }
-}
+/* /1* Change styles for span and cancel button on extra small screens *1/ */
+/* @media screen and (max-width: 300px) { */
+/*     span.psw { */
+/*         display: block; */
+/*         float: none; */
+/*     } */
+/*     .cancelbtn { */
+/*         width: 100%; */
+/*     } */
+/* } */
 
-</style>
+/* </style> */
 </head>
 <body>
 <?php
@@ -126,26 +123,31 @@ span.psw {
     } 
 ?>
 
-<div style="margin-top:43px">
- <form action="login.php" method="POST">
-
-  <div>
+<div class="row">
+ <form class="col s12" action="login.php" method="POST">
+    <div class="row">
     <label for="uname"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="uname" required>
+    <input type="text" placeholder="Enter Username" name="uname" class="validate">
+    </div>
 
+    <div class="row">
     <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
+    <input type="password" placeholder="Enter Password" name="psw" class="validate">
+    </div>
+    <div class="row">
+        <span class="psw">Forgot <a href="#">password?</a></span>
+        <label>
+            <input type="checkbox" name="remember"> Remember me
+        </label>
+    </div>
 
-    <button type="submit" name= "login">Login</button>
-    <label>
-      <input type="checkbox" checked="checked" name="remember"> Remember me
-    </label>
-  </div>
-
-  <div class="container" style="background-color:#f1f1f1">
+    <div class="row">
+  <button class="btn waves-effect waves-light" type="submit" name="login">Login</button>
+    </div>
+        
+    <div class="row">
     <button type="button" class="cancelbtn">Cancel</button>
-    <span class="psw">Forgot <a href="#">password?</a></span>
-  </div>
+    </div>
 </div>
 </form> 
 <?php
