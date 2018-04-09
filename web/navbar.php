3fbe1262e1da3a301fe357dd7e48772da1e48861
derrick <derrick.2a4@gmail.com>
2018-04-09 14:18:10 +0800

    Remove old navbar

--- web/navbar.php
+++ web/navbar.php
@@ -29,28 +29,3 @@
       <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
     </div>
   </nav>
-
-<div class="w3-top">
-  <div class="w3-bar w3-theme w3-top w3-left-align w3-large">
-    <a href="index.php" class="w3-bar-item w3-button w3-theme-l1">Stuffsharing</a>
-    <!-- Right-sided navbar links -->
-    <div class="w3-right w3-hide-small">
-    <?php
-        if (isset($_SESSION['isAdmin']) && $_SESSION['isAdmin']) {
-            echo '<a href="admin.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Admin Panel</a>';
-        } else {
-
-        }
-        if (!isset($_SESSION['user'])) {
-            echo '<a href="login.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Login</a>
-    <a href="signup.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Sign Up</a>';
-    } else {
-        echo '<a href="userloanout.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white">My Loans</a>';
-        echo '<a href="bids.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white">My Bids</a>';
-        echo '<a href="logout.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Logout</a>';
-    }
-    ?>
-    </div>
-  </div>
-</div>
-
