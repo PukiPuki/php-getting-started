40196debde3fe0ab780d356f914da56ac7be2d03
derrick <derrick.2a4@gmail.com>
2018-04-10 16:03:39 +0800

    Try fix css

--- web/signup.php
+++ web/signup.php
@@ -45,6 +45,16 @@
         }
     }
 ?>
+  <div class="row">
+    <form class="col s12">
+      <div class="row">
+        <div class="input-field col s12">
+          <textarea id="textarea1" class="materialize-textarea"></textarea>
+          <label for="textarea1">Textarea</label>
+        </div>
+      </div>
+    </form>
+  </div><Paste>
 
 <div id="input" class="row">
     <form action="signup.php" method="POST" class="col s12">
