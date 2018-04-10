fc2719b847056a67da4d05d37ad278d9a0035699
derrick <derrick.2a4@gmail.com>
2018-04-10 15:24:30 +0800

    Try fix css

--- web/signup.php
+++ web/signup.php
@@ -88,5 +88,7 @@
 <?php
     echo $message;
 ?>
+ <!--JavaScript at end of body for optimized loading-->
+      <script type="text/javascript" src="./style/js/materialize.min.js"></script>
 </body>
 </html>
