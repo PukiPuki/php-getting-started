f28c917138018756105a9556b1d8a6a1d818ca68
derrick <derrick.2a4@gmail.com>
2018-04-11 14:59:21 +0800

    Fix javascript

--- web/index.php
+++ web/index.php
@@ -244,6 +244,8 @@ END;
       </div>
     </div>
   </footer>
+ <!--JavaScript at end of body for optimized loading-->
+      <script type="text/javascript" src="./style/js/materialize.min.js"></script>
 
 </body>
 </html>
