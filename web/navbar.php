ac78224a73f7d934f12b132d360c2206e90f63ad
derrick <derrick.2a4@gmail.com>
2018-04-11 16:21:40 +0800

    Edit navbar

--- web/navbar.php
+++ web/navbar.php
@@ -2,29 +2,30 @@
 <nav class="light-blue lighten-1" role="navigation">
     <div class="nav-wrapper container"><a id="logo-container" href="/" class="brand-logo site-logo">Stuffsharing</a>
     <?php
-        if (isset($_SESSION['isAdmin']) && $_SESSION['isAdmin']) {
-            echo '<ul class="right hide-on-med-and-down"> 
-                    <li> <a href="admin.php" >Admin Panel</a></li>
-                    </ul>';
-        } 
         if (!isset($_SESSION['user'])) {
             echo '<ul class="right hide-on-med-and-down"> 
                     <li> <a href="login.php" >Login</a></li>
                     </ul>
                 <ul class="right hide-on-med-and-down"> 
                     <li> <a href="signup.php"> Sign Up </a></li>
-                    </ul>';
-    } else {
-            echo '<ul class="right hide-on-med-and-down"> 
-                        <li> <a href="userloanout.php" >My Loans</a></li>
-                    </ul>
+                    </ul>'; 
+        } else {
+            echo '
+                <ul class="right hide-on-med-and-down"> 
+                        <li> <a href="logout.php">Logout</a></li>
+                </ul>
                     <ul class="right hide-on-med-and-down"> 
                         <li> <a href="bids.php">My Bids</a></li>
                     </ul>
                     <ul class="right hide-on-med-and-down"> 
-                        <li> <a href="logout.php">Logout</a></li>
+                        <li> <a href="userloanout.php" >My Loans</a></li>
                     </ul>';
-    }
+        }
+        if (isset($_SESSION['isAdmin']) && $_SESSION['isAdmin']) {
+            echo '<ul class="right hide-on-med-and-down"> 
+                    <li> <a href="admin.php" >Admin Panel</a></li>
+                    </ul>';
+        } 
     ?>
     </div>
 </nav>
