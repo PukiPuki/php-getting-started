9e2557c2aeb5f6390ccfadba9e79061302fff29a
derrick <derrick.2a4@gmail.com>
2018-04-11 17:35:55 +0800

    Fix js

--- web/admin.php
+++ web/admin.php
@@ -3,15 +3,11 @@
 session_start();
 include 'pgconnect.php';
 include 'refresh.php';
+include 'checkadmin.php';
 ?>
 
 <html>
 <head>
-    <script>
-        var elem = document.querySelector('.collapsible');
-        var instance = M.Collapsible.init(elem, options);
-    </script>
-
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Compiled and minified CSS -->
@@ -258,7 +254,7 @@ include 'navbar.php';
 </div>
 <!--JavaScript at end of body for optimized loading-->
       <script type = "text/javascript" src = "https://code.jquery.com/jquery-2.1.1.min.js"></script>           
-      <script src = "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/js/materialize.min.js">
+      <script src = "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/js/materialize.min.js"></script>
 
 </body>
 </html>
