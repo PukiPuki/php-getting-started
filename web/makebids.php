e66fc9ba8681685a4bf23219599ddd871aafa6e7
derrick <derrick.2a4@gmail.com>
2018-04-11 15:29:28 +0800

    Fix bids

--- web/makebids.php
+++ web/makebids.php
@@ -3,7 +3,10 @@ function makeBidInput($string) {
     return
         '
             <form action="index.php" method="POST" class="col s6">
-                 <input class="col s6" type="text" placeholder="' . $string . '" name="new_bid" required>
+                <input type="hidden" value="'. $row["tid"].'">
+                <div class="input-field inline">
+                 <input class="col s6" type="text" value="' . $string . '" name="new_bid" required>
+                </div>
                  <button class="btn waves-effect wave-light" type="submit" name="tid" value="' . $string . '">Bid</button>
             </form>
         ';
