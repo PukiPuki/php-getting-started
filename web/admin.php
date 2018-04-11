73d5bcc90ef4ce00a49699156af5538337bf7570
derrick <derrick.2a4@gmail.com>
2018-04-11 16:13:49 +0800

    Try collapsible

--- web/admin.php
+++ web/admin.php
@@ -7,85 +7,11 @@ include 'refresh.php';
 
 <html>
 <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <style>
-        form {
-            border: 3px solid #f1f1f1;
-        }
-
-        /* Full-width inputs */
-        input[type=text], input[type=password] {
-            width: 100%;
-            padding: 12px 20px;
-            margin: 8px 0;
-            display: inline-block;
-            border: 1px solid #ccc;
-            box-sizing: border-box;
-        }
-
-        /* Set a style for all buttons */
-        button {
-            background-color: #4CAF50;
-            color: white;
-            padding: 14px 20px;
-            margin: 8px 0;
-            border: none;
-            cursor: pointer;
-            width: 100%;
-        }
-
-        /* Add a hover effect for buttons */
-        button:hover {
-            opacity: 0.8;
-        }
-
-        /* Extra style for the cancel button (red) */
-        .cancelbtn {
-            width: auto;
-            padding: 10px 18px;
-            background-color: #f44336;
-        }
-
-        /* Center the avatar image inside this container */
-        .imgcontainer {
-            text-align: center;
-            margin: 24px 0 12px 0;
-        }
-
-        /* Avatar image */
-        img.avatar {
-            width: 40%;
-            border-radius: 50%;
-        }
-
-        /* Add padding to containers */
-        .container {
-            padding: 16px;
-        }
-
-        /* The "Forgot password" text */
-        span.psw {
-            float: right;
-            padding-top: 16px;
-        }
-
-        /* Change styles for span and cancel button on extra small screens */
-        @media screen and (max-width: 300px) {
-            span.psw {
-                display: block;
-                float: none;
-            }
-
-            .cancelbtn {
-                width: 100%;
-            }
-        }
-
-    </style>
+    <!-- Compiled and minified CSS -->
+    <link type="text/css" rel="stylesheet" href="./style/css/materialize.min.css" media="screen,projection">
+    <link href="https://fonts.googleapis.com/icon?family=Inconsolata" rel="stylesheet">
 </head>
 <body>
 <?php
@@ -93,57 +19,69 @@ include 'navbar.php';
 
 ?>
 
-<div style="margin-top:43px">
-    <div>
-        <h1 class="w3-text-teal"> User Control </h1>
+<div>
+    <div class="container">
+        <h1 class="red"> User Control </h1>
 
-        <div>
-            <h3 class="w3-text-teal"> Add User </h3>
-            <form action="admin_users.php" method="POST">
-                <div>
-                    <label for="username"><b>Username</b></label>
-                    <input type="text" placeholder="Enter Username" name="username" required>
-                    <label for="password"><b>Password</b></label>
-                    <input type="password" placeholder="Enter Password" name="password" required>
-                    <label for="phone"><b>Phone Number</b></label>
-                    <input type="text" placeholder="Enter Phone Number" name="phone" required>
-                    <label for="isAdmin"><b>Admin Privileges</b></label>
-                    <input type="hidden" name="isAdmin" value="0"> </input>
-                    <input type="checkbox" id="isAdmin" name="isAdmin" value="1"> 
-                        <label for="isAdmin" >
-                            <span> Admin </span>
-                        </label>
-                    <button type="submit" name="add_user">Add User</button>
-                </div>
-            </form>
-        </div>
+  <ul class="collapsible">
+    <li>
+      <div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
+      <div class="collapsible-body">
+        <h3 class="green"> Add User </h3>
+             <form action="admin_users.php" method="POST">
+                 <div>
+                     <label for="username"><b>Username</b></label>
+                     <input type="text" placeholder="Enter Username" name="username" required>
+                     <label for="password"><b>Password</b></label>
+                     <input type="password" placeholder="Enter Password" name="password" required>
+                     <label for="phone"><b>Phone Number</b></label>
+                     <input type="text" placeholder="Enter Phone Number" name="phone" required>
+                     <label for="isAdmin"><b>Admin Privileges</b></label>
+                     <input type="hidden" name="isAdmin" value="0"> </input>
+                     <input type="checkbox" id="isAdmin" name="isAdmin" value="1"> 
+                         <label for="isAdmin" >
+                             <span> Admin </span>
+                         </label>
+                     <button type="submit" name="add_user">Add User</button>
+                 </div>
+             </form>
+      </div>
+    </li>
+    <li>
+      <div class="collapsible-header"><i class="material-icons">place</i>Second</div>
+      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+    </li>
+    <li>
+      <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
+      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+    </li>
+         <form action="admin_users.php" method="POST">
+             <div>
+                 <h2 class="w3-text-teal"> Edit User </h2>
+                 <label for="username"><b>Username</b></label>
+                 <input type="text" placeholder="Enter User to Edit" name="username" required>
 
-        <form action="admin_users.php" method="POST">
-            <div>
-                <h2 class="w3-text-teal"> Edit User </h2>
-                <label for="username"><b>Username</b></label>
-                <input type="text" placeholder="Enter User to Edit" name="username" required>
+                 <label for="newphone"><b>New Phone Number</b></label>
+                 <input type="text" placeholder="Enter New Phone Number" name="newphone" required>
 
-                <label for="newphone"><b>New Phone Number</b></label>
-                <input type="text" placeholder="Enter New Phone Number" name="newphone" required>
+                 <label for="isAdmin"><b>Admin Privileges</b></label>
+                 <input type="hidden" name="isAdmin" value="0"> </input>
+                 <input type="checkbox" name="isAdmin" value="1"> Admin </input>
+                 <button type="submit" name="edit_user">Edit User</button>
+             </div>
+         </form>
 
-                <label for="isAdmin"><b>Admin Privileges</b></label>
-                <input type="hidden" name="isAdmin" value="0"> </input>
-                <input type="checkbox" name="isAdmin" value="1"> Admin </input>
-                <button type="submit" name="edit_user">Edit User</button>
-            </div>
-        </form>
-
-        <div>
-            <h2 class="w3-text-teal"> Remove User </h2>
-            <form action="admin_users.php" method="POST">
-                <div>
-                    <label for="username"><b>Username</b></label>
-                    <input type="text" placeholder="Enter User to Remove" name="username" required>
-                    <button type="submit" name="remove_user">Remove User</button>
-                </div>
-            </form>
-        </div>
+         <div>
+             <h2 class="w3-text-teal"> Remove User </h2>
+             <form action="admin_users.php" method="POST">
+                 <div>
+                     <label for="username"><b>Username</b></label>
+                     <input type="text" placeholder="Enter User to Remove" name="username" required>
+                     <button type="submit" name="remove_user">Remove User</button>
+                 </div>
+             </form>
+         </div>
+     </ul>
     </div>
 <!-- For bids-->
 
