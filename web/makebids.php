037e0c75227eab1dceb7c85c92b477e4329aa021
derrick <derrick.2a4@gmail.com>
2018-04-11 15:37:47 +0800

    fix bids

--- web/makebids.php
+++ web/makebids.php
@@ -3,7 +3,7 @@ function makeBidInput($string, $tid) {
     return
         '
             <form action="index.php" method="POST" class="col s6">
-                <input type="hidden" value="'. $tid .'">
+                <input type="hidden" value="'. $tid .'" />
                 <div class="input-field inline">
                  <input class="col s6" type="text" value="' . $string . '" name="new_bid" required>
                 </div>
