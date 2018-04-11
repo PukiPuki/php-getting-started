154c39a1d2f696d968a6827184de30617f20ad96
derrick <derrick.2a4@gmail.com>
2018-04-11 14:33:02 +0800

    Fix css

--- web/makebids.php
+++ web/makebids.php
@@ -2,8 +2,8 @@
 function makeBidInput($string) {
     return
         '
-            <form action="index.php" method="POST">
-                 <input class="col s2" type="text" placeholder="' . $string . '" name="new_bid" required>
+            <form action="index.php" method="POST" class="col s6">
+                 <input class="col s6" type="text" placeholder="' . $string . '" name="new_bid" required>
                  <button class="btn waves-effect wave-light" type="submit" name="tid" value="' . $string . '">Bid</button>
             </form>
         ';
