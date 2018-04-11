270c1d29e2da37b57ffea17d3c056e86970e3f14
derrick <derrick.2a4@gmail.com>
2018-04-11 16:23:16 +0800

    Change admin

--- web/admin.php
+++ web/admin.php
@@ -247,5 +247,7 @@ include 'navbar.php';
 ?>
             </div>
 </div>
+ <!--JavaScript at end of body for optimized loading-->
+      <script type="text/javascript" src="./style/js/materialize.min.js"></script>
 </body>
 </html>
